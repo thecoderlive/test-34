d8ed391c885d06cd1d493a19c651059eaaeb5f1d
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 01:39:52 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "f84573db-660d-43d3-b8a7-d39eaf14309d",
+    "home_feed": [
+        {
+            "id": "e2dcfbff-ba53-4980-91a5-ef7c5ae7cbfb",
+            "artist_name": "friend",
+            "home_carousel": [
+                {
+                    "id": "bbd56b59-d648-4ec3-aab0-d437105464c1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                },
+                {
+                    "id": "73b3d7ae-89af-4d1e-99d1-f018268ac8d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "16527b42-0d54-49fc-b98f-45650e7b64ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                },
+                {
+                    "id": "d4ed536a-38f0-4812-8668-65c718895e35",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "97b75539-1596-44e6-ba98-52047ae1e2d5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png"
+                }
+            ],
+            "art_caption": "health",
+            "art_description": "Fish best beautiful scene. Small technology manager Mr it station."
+        },
+        {
+            "id": "7e785f8f-9456-4206-9974-676f5770b7d0",
+            "artist_name": "down",
+            "home_carousel": [
+                {
+                    "id": "0d0579df-f572-4fc9-a6a5-bc1b2860a500",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                },
+                {
+                    "id": "e5373f1c-ee41-4c5c-8493-f24d2fe25259",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "c19c1ffb-60b3-4bb5-912f-4ea19d26c897",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "d71612fe-a777-4d95-844f-e31bcb25ae12",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "e36c593f-b762-4957-9349-419706ebc6bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                }
+            ],
+            "art_caption": "position",
+            "art_description": "Letter evening college success stop have best process. Industry remember church have eat."
+        },
+        {
+            "id": "1b2f6db7-fefc-4154-8aa2-0124c667dea1",
+            "artist_name": "away",
+            "home_carousel": [
+                {
+                    "id": "ba5423db-44bb-4a04-96e2-ce7abef4f9d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                },
+                {
+                    "id": "36b357d2-66fe-4780-874b-c124dacb2535",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "d8fbf2fe-de5b-4e39-b708-41f80c9672fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+                },
+                {
+                    "id": "70256aa6-aaad-47d7-a1bf-634d00a87227",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "cff4b5a2-5c10-47c4-8cea-b00843965eba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                }
+            ],
+            "art_caption": "charge",
+            "art_description": "Trouble example area star girl daughter matter education. Could try also as those. Seat culture reflect significant shake everything article."
+        },
+        {
+            "id": "903e68b2-95f9-40c7-813b-dc6bc3f8bffa",
+            "artist_name": "tax",
+            "home_carousel": [
+                {
+                    "id": "63e31b57-2a9e-48ba-814f-36234433ad61",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "3f6317c0-9609-435a-bd57-c5df83e3266f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                },
+                {
+                    "id": "0c0fe7eb-00cc-4d5e-99b0-aab8323d6956",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                },
+                {
+                    "id": "10ac5b4f-65a6-44fc-ace7-e1c3c6d22252",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/301.png"
+                },
+                {
+                    "id": "9eead7fd-0beb-43cb-aa64-abb72c269ffa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                }
+            ],
+            "art_caption": "attention",
+            "art_description": "Easy well point short low quality. Main music evidence wonder political. Give point real friend."
+        },
+        {
+            "id": "bfff4f24-7cdb-41ef-ad8f-1d2a01c46a5b",
+            "artist_name": "should",
+            "home_carousel": [
+                {
+                    "id": "7a9bc6a4-e3f7-4432-8c7e-21efa5d1bc4d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "bb723977-9f20-4ffe-b657-d274921044b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "26bfa01e-627c-49f6-9520-4213a130c08b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/301.png"
+                },
+                {
+                    "id": "f9b21d7a-b183-4fd5-8db6-88dfa8c79a12",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "5e700045-dc1c-4069-a152-c095f3b71d56",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                }
+            ],
+            "art_caption": "human",
+            "art_description": "Country six away themselves early. Especially agency them whose. Family experience college doctor others focus baby citizen. All street meet."
+        }
+    ]
+}
